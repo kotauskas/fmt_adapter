40b2f766160f8e39a025503d7410e26c194300b4
Kotauskas <v.toncharov@gmail.com>
2020-03-26 13:58:19 +0300

    Hotfix

--- src/generated/mod.rs
+++ src/generated/mod.rs
@@ -1,6 +1,6 @@
 // This file is NOT GENERATED. It only exists to export modules from the generated subfolder.
 
-mod onlystructs;
-pub use onlystructs::*;
+mod only_adapters;
+pub use only_adapters::*;
 mod fmt_adapters;
 pub use fmt_adapters::*;
